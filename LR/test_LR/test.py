4276aded020cb916a0a632bea13090135e023665
Akash <akashadike15@gmail.com>
2024-04-29 11:42:53 +0530

    trained a linear regression model manually using tensors

--- LR/test_LR/test.py
+++ LR/test_LR/test.py
@@ -1,2 +1,15 @@
 import numpy as np
-print(np.zeros(5))
+import torch
+
+a=np.array([1,2,3,4])
+a=torch.from_numpy(a)
+
+b=np.array([2,2,2,2])
+b=torch.from_numpy(b)
+
+print(np.dot(a,b))
+
+m=[[1,2,3],[4,5,6]]
+m=torch.tensor(m)
+x,y=m.shape
+print(x,y)
